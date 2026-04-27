a868b35c7358caa465a571cf696107cf84d6d175
deiby sierra <desierra@activos.com.co>
2026-04-27 18:16:18 -0500

    creacion PersonNotFoundException

---
+++ src/main/java/com/co/activos/msel0001/domain/exceptions/PersonNotFoundException.java
@@ -0,0 +1,7 @@
+package com.co.activos.msel0001.domain.exceptions;
+
+public class PersonNotFoundException extends RuntimeException {
+    public PersonNotFoundException(String message) {
+        super(message);
+    }
+}
